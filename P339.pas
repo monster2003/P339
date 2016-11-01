1af5b40da2617eaea5a7fc797239b5393199eacb
monster2003 <v.alex.4mail@gmail.com>
2016-11-01 18:53:08 +0300

    Найдите самый маленький натуральный делитель числа x, отличный от 1 (2 <= x <= 30000).
    Входные данные
    Вводится натуральное число x.
    
    Выходные данные
    Выведите наименьший делитель числа x, отличный от 1.

---
+++ P339.pas
@@ -0,0 +1,17 @@
+(*
+ * Project: P339
+ * User: Alex
+ * Date: 01.11.2016
+ *)
+program P339;
+var
+    x,i:Integer ;
+begin
+    ReadLn(x);
+    for i:=1 to x do
+    begin
+        if (x mod i)=0 then
+    end;
+    WriteLn(i);
+
+end.
